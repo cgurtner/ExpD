9af0d1105d9608cf658c1daf1e1ac38db31a2a69
Cyrill Gurtner <me@cgurtner.ch>
2022-09-26 14:49:06 +0200

    P02/aufgabe2.R

---
+++ P02/aufgabe2.R
@@ -0,0 +1,16 @@
+# a)
+
+a <- seq(from=95, to=-5, by=-5)a
+a
+
+abc <- letters
+rep(abc, each=2)
+
+nums <- c(1:10)
+rep(nums, times=nums)
+
+# b)
+
+mx_sq <- seq(from=3, to=75, by=3)
+mx <- matrix(mx_sq, nrow=5, ncol=5, byrow=TRUE)
+mx
